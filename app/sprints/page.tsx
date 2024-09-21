61b2c782b2165001c9626b34f11c28d08ca8d12e
OverkillLC <97358665+OverkillLC@users.noreply.github.com>
2024-09-21 18:06:02 +0000

    Added content for sprint page

--- app/sprints/page.tsx
+++ app/sprints/page.tsx
@@ -1,11 +1,31 @@
 "use client"; // This directive allows the component to run on the client side.
-import { Box, Container, Link, Stack, Typography } from "@mui/material";
+import { Box, Container, Link, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import ResponsiveAppBar from "@/components/navbar";
 
 export default function Sprints() {
   return (
     <Box>
       <ResponsiveAppBar />
+      <h2>Sprint 1</h2>
+      <Table sx={{ maxWidth: 650 }} aria-label="Example static collection table">
+        <TableHead>
+          <TableRow>
+          <TableCell>
+            <Box sx={{ p: 2, border: '1px dashed grey', width: 200 }}>
+              Split team into two groups
+            </Box>
+            </TableCell>
+
+            <TableCell>
+            <Box sx={{ p: 2, border: '1px dashed grey', width: 200 }}>
+              Review original codebase
+            </Box>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        </Table>
+  
+        
     </Box>
   );
 }
