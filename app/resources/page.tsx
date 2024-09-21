ae1288c402e4c0e11774c440667430eabef26af5
OverkillLC <97358665+OverkillLC@users.noreply.github.com>
2024-09-21 17:40:26 +0000

    Added content for Resources and Time Tracking pages

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,11 +1,21 @@
 "use client"; // This directive allows the component to run on the client side.
-import { Box, Container, Link, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Link, Stack, Typography } from "@mui/material";
 import ResponsiveAppBar from "@/components/navbar";
 
 export default function Resources() {
   return (
     <Box>
       <ResponsiveAppBar />
+      <Box sx={{ textAlign: "left", pt: "5vh" }}>
+        <h2>Week 1</h2>
+        <Button variant="contained" href="https://docs.google.com/presentation/d/1E1ebly_8aZ3sP5-ta5DjYG2ETjui0gxEugu2oE9HEUo/edit?usp=sharing">
+              Week 1 Slides
+        </Button>
+        <h2>Week 2</h2>
+        <Button variant="contained" href="https://docs.google.com/presentation/d/1QDEsVZXuEm_hUe95__Pe__K4cmiR8G0bxx8NeyYBHPo/edit?usp=sharing">
+              Week 2 Slides
+        </Button>
+      </Box>
     </Box>
   );
 }
--- app/time/page.tsx
+++ app/time/page.tsx
@@ -1,11 +1,46 @@
 "use client"; // This directive allows the component to run on the client side.
-import { Box, Container, Link, Stack, Typography } from "@mui/material";
+import { Box, Container, Link, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import ResponsiveAppBar from "@/components/navbar";
 
 export default function SignUpPage() {
   return (
     <Box>
       <ResponsiveAppBar />
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Week</TableCell>
+            <TableCell align="center">Owen</TableCell>
+            <TableCell align="center">Michael</TableCell>
+            <TableCell align="center">Arnav</TableCell>
+            <TableCell align="center">Ryan</TableCell>
+            <TableCell align="center">Jeffrey</TableCell>
+            <TableCell align="center">Terrence</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell align="center">2</TableCell>
+            <TableCell align="center">2</TableCell>
+            <TableCell align="center">2</TableCell>
+            <TableCell align="center">2</TableCell>
+            <TableCell align="center">2</TableCell>
+            <TableCell align="center">2</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>2</TableCell>
+            <TableCell align="center">8.92</TableCell>
+            <TableCell align="center">3</TableCell>
+            <TableCell align="center">3.5</TableCell>
+            <TableCell align="center">4</TableCell>
+            <TableCell align="center">3</TableCell>
+            <TableCell align="center">3</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
     </Box>
   );
 }
