6a113f6c635f015d0c19b6288fb3acc99470cf9d
OverkillLC <97358665+OverkillLC@users.noreply.github.com>
2024-09-21 19:07:47 +0000

    added links to repos

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,12 +1,22 @@
 "use client"; // This directive allows the component to run on the client side.
 import { Box, Button, Container, Link, Stack, Typography } from "@mui/material";
 import ResponsiveAppBar from "@/components/navbar";
-
+import "../globals.css"
+//https://github.com/SE4AIResearch/DataLeakage_JupyterNotebook_Website_Fall2024/
 export default function Resources() {
   return (
     <Box>
       <ResponsiveAppBar />
       <Box sx={{ textAlign: "left", pt: "5vh" }}>
+        <h2>Source Code</h2>
+        <Button variant="contained" href="https://github.com/SE4AIResearch/DataLeakage_JupyterNotebook_Fall2024/">
+              Plugin Code
+        </Button>
+        <br></br>
+        <br></br>
+        <Button variant="contained" href="https://github.com/SE4AIResearch/DataLeakage_JupyterNotebook_Website_Fall2024/">
+              Website Code
+        </Button>
         <h2>Week 1</h2>
         <Button variant="contained" href="https://docs.google.com/presentation/d/1E1ebly_8aZ3sP5-ta5DjYG2ETjui0gxEugu2oE9HEUo/edit?usp=sharing">
               Week 1 Slides
