8dc20fe99c7a3f40001542a8426e4a9ec76c2796
OverkillLC <97358665+OverkillLC@users.noreply.github.com>
2024-09-21 18:57:37 +0000

    added credits

--- app/sprints/page.tsx
+++ app/sprints/page.tsx
@@ -1,3 +1,4 @@
+//Based on code from https://github.com/SE4AIResearch/AntiCopyPaster_Website_Fall2023/tree/main
 "use client"; // This directive allows the component to run on the client side.
 import { Box, Container, Link, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import ResponsiveAppBar from "@/components/navbar";
@@ -8,75 +9,6 @@ export default function Sprints() {
   return (
     <Box>
       <ResponsiveAppBar />
-      {/* <h2>Sprint 1</h2>
-      <Table sx={{ maxWidth: 650 }} aria-label="Example static collection table">
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 250, height: 120 }}>
-                <h3>Split team into two group.</h3>
-                Members Involved: All
-                <p>Done</p>
-              </Box>
-              </TableCell>
-
-              <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 250, height: 120 }}>
-                <h3>Review original codebase</h3>
-                Members Involved: All
-                <p>Done</p>
-              </Box>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-      </Table>
-      <h2>Sprint 2</h2>
-      <Table sx={{ maxWidth: 650 }} aria-label="Example static collection table">
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 250, height: 120 }}>
-                <h3>Read Fall 2023 Report</h3>
-                Members Involved: All
-                <p>Done</p>
-              </Box>
-            </TableCell>
-
-            <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 250, height: 120 }}>
-                <span>Done</span>
-                <h3>Install Docker</h3>
-                Members Involved: All
-                
-              </Box>
-            </TableCell>
-
-            <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 250, height: 120 }}>
-                <h3>Create Short Quiz</h3>
-                Members Involved: Owen
-                <p>Done</p>
-              </Box>
-            </TableCell>
-
-            <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 400, height: 120 }}>
-                <h3>Complete Short Quiz</h3>
-                Members Involved: Michael, Terrence, Arnav, Ryan, Jeffrey
-                <p>Done</p>
-              </Box>
-            </TableCell>
-
-            <TableCell>
-              <Box sx={{ p: 1, border: '1px dashed grey', width: 250, height: 120 }}>
-                <h3>Create Test VSCode Extension</h3>
-                 Members Involved: All
-                <p>Done</p>
-              </Box>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-      </Table> */}
 
       <div className="bubble">
         <h1>Sprint 1</h1>
