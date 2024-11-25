2952918be5cbe0e862b170206f972366b2aecbec
Ryan Lee <ryanoaklandgardens@gmail.com>
2024-11-24 23:38:20 -0500

    quick refactor

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -19,7 +19,6 @@ export default function Download() {
             startIcon={<MicrosoftIcon />}
             href="/binaries/windows-x64.zip"
             sx={{ fontSize: "1.2rem", textTransform: "none" }}
-            download
           >
             Windows-x64
           </Button>
@@ -29,19 +28,10 @@ export default function Download() {
             startIcon={<AppleIcon />}
             href="/binaries/macos14-arm64.zip"
             sx={{ fontSize: "1.2rem", textTransform: "none" }}
-            download
           >
             MacOS14-ARM64
           </Button>
         </Stack>
-        {/* <Button
-          variant="contained"
-          color="info"
-          href="https://stevens0-my.sharepoint.com/:f:/g/personal/ealomar_stevens_edu/EoM72dehZWZClpFgI1-0IkUB7Uaq_lNdLejGz8rrI3bVQw?e=PQdtaT"
-          sx={{ mb: 2, fontSize: "1.2rem", textTransform: "none" }}
-        >
-          Go to Binaries
-        </Button> */}
       </Container>
     </Box>
   );
